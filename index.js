31c6d6194f9823f8b07f488317db63f039978db6
Stacy Lee <stacy.lee92@gmail.com>
2025-04-24 15:56:47 -0700

    need-help

---
+++ index.js
@@ -0,0 +1,20 @@
+let userInput = console.log(
+  prompt("Enter a list of comma-separated froyo flavors:")
+);
+
+// for (i = 0; i < userInput.length; i++) {
+//   console.log(userInput[i]);
+// }
+
+// let count = 0;
+// for (let i = 0; i < userInput.length; i++) {
+//   if (flavors[i] === ",") {
+//     count += flavors;
+//   }
+// }
+
+// console.log(`You have ordered ${flavors}`);
+
+// convert the 'flavors' input into an array
+//count the number of flavors for each value
+//console.log template literal for the number of flavors of each
