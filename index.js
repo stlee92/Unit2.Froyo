75c0eb22c8ca1118a514543e8229cdffd0c9cb17
Stacy Lee <stacy.lee92@gmail.com>
2025-04-24 18:48:11 -0700

    updates

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-let userInput = console.log(
+// let userInput = console.log(
+//   prompt("Enter a list of comma-separated froyo flavors:")
+// );
+
+const userInput = console.log(
   prompt("Enter a list of comma-separated froyo flavors:")
 );
+
+const flavors = [];
+function convertInputToArray(userInput) {
+  for (const flavors of userInput) {
+    const flavors = userInput.split(",");
+  }
+  return flavors;
+}
+console.log(userInput);
 
 // for (i = 0; i < userInput.length; i++) {
 //   console.log(userInput[i]);
@@ -16,5 +29,7 @@ let userInput = console.log(
 // console.log(`You have ordered ${flavors}`);
 
 // convert the 'flavors' input into an array
+//Block12 YT playlist 1:50:11
+
 //count the number of flavors for each value
 //console.log template literal for the number of flavors of each
